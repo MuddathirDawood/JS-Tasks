adc52973ca7389371e46170636d3c20cd7911826
MuddathirDawood <dmuddathir@gmail.com>
2022-06-17 10:15:07 +0200

    nextNumber

---
+++ nextNumber/script/code.js
@@ -0,0 +1,6 @@
+let array = [8,7,5,3,1,6,9,4,2];
+
+
+document.querySelector('#array').innerHTML = array;
+document.querySelector('#arr2').innerHTML = array[2];
+document.querySelector('#arr5').innerHTML = array[5];
